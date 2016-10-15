4d4dc799ae2c089813f23ce742b2b3a6c05d0f79
Walker Hannan <awahannan@gmail.com>
2016-10-15 18:54:28 -0400

    Add initial api and client files

---
+++ api/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mgck8ball
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ api/test/mgck8ball/core_test.clj
@@ -0,0 +1,7 @@
+(ns mgck8ball.core-test
+  (:require [clojure.test :refer :all]
+            [mgck8ball.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
